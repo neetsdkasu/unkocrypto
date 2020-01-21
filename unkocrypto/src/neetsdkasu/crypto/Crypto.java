110d3b9776fc0eec5a6858c36cf8f1ef30b9c46e
Leonardone <neetsdkasu@gmail.com>
2020-01-21 19:04:03 +0900

    処理を書き直した。コンパイルは通るが未テスト、REする可能性あり

--- unkocrypto/src/neetsdkasu/crypto/Crypto.java
+++ unkocrypto/src/neetsdkasu/crypto/Crypto.java
@@ -6,26 +6,27 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.util.Random;
-import neetsdkasu.util.zip.Checksum;
+import java.util.zip.Checksum;
 
 public final class Crypto
 {
-    public static final int MIN_SIZE = 32;
-    public static final int MAX_SIZE = 1024;
-    public static final int META_SIZE = (/* Integer.SIZE */ 32 / 8) + (/* Long.SIZE */ 64 / 8); // SIZE is nothing CLDC/MIDP
+    public static final int MIN_BLOCKSIZE = 32;
+    public static final int MAX_BLOCKSIZE = 1024;
+    public static final int META_SIZE = (Integer.SIZE / 8) + (Long.SIZE / 8);
+    private static final int BYTE = 0x100; // 256
+    private static final int MASK = 0x0FF; // 255
 
     private Crypto() {
-        // throw new UnsupportedOperationException();
-        throw new RuntimeException("UnsupportedOperationException");
+        throw new UnsupportedOperationException();
     }
 
-    private static void validateParams(int size, Checksum checksum, Random rand, byte[] src)
+    private static void validateParams(int blockSize, Checksum checksum, Random rand, byte[] src)
     {
-        if (size < MIN_SIZE || MAX_SIZE < size)
+        if (blockSize < MIN_BLOCKSIZE || MAX_BLOCKSIZE < blockSize)
         {
             throw new IllegalArgumentException(
-                "OUT OF RANGE size(" + size + "): "
-                + MIN_SIZE + " <= size <= " + MAX_SIZE);
+                "OUT OF RANGE blockSize(" + blockSize + "): "
+                + MIN_BLOCKSIZE + " <= blockSize <= " + MAX_BLOCKSIZE);
         }
         if (checksum == null)
         {
@@ -41,98 +42,105 @@ public final class Crypto
         }
     }
 
-    public static byte[] decrypt(int size, Checksum checksum, Random rand, byte[] src)
+    public static byte[] decrypt(int blockSize, Checksum checksum, Random rand, byte[] src)
     {
-        validateParams(size, checksum, rand, src);
-        return doDecrypt(size, checksum, rand, src, 0);
+        validateParams(blockSize, checksum, rand, src);
+        return doDecrypt(blockSize, checksum, rand, src, 0, src.length);
     }
 
-    public static byte[] decrypt(int size, Checksum checksum, Random rand, byte[] src, int off)
+    public static byte[] decrypt(int blockSize, Checksum checksum, Random rand, byte[] src, int fromIndex, int toIndex)
     {
-        validateParams(size, checksum, rand, src);
-        return doDecrypt(size, checksum, rand, src, off);
+        validateParams(blockSize, checksum, rand, src);
+        return doDecrypt(blockSize, checksum, rand, src, fromIndex, toIndex);
     }
 
-    public static byte[] encrypt(int size, Checksum checksum, Random rand, byte[] src)
+    public static byte[] encrypt(int blockSize, Checksum checksum, Random rand, byte[] src)
     {
-        validateParams(size, checksum, rand, src);
-        return doEncrypt(size, checksum, rand, src, 0, src.length);
+        validateParams(blockSize, checksum, rand, src);
+        return doEncrypt(blockSize, checksum, rand, src, 0, src.length);
     }
 
-    public static byte[] encrypt(int size, Checksum checksum, Random rand, byte[] src, int off, int len)
+    public static byte[] encrypt(int blockSize, Checksum checksum, Random rand, byte[] src, int fromIndex, int toIndex)
     {
-        validateParams(size, checksum, rand, src);
-        return doEncrypt(size, checksum, rand, src, off, len);
+        validateParams(blockSize, checksum, rand, src);
+        return doEncrypt(blockSize, checksum, rand, src, fromIndex, toIndex);
     }
 
-    private static byte[] doDecrypt(int size, Checksum checksum, Random rand, byte[] src, int off)
+    private static byte[] doDecrypt(int blockSize, Checksum checksum, Random rand, byte[] src, int fromIndex, int toIndex)
     {
         try
         {
-            if (off < 0 || src.length <= off)
+            if (fromIndex < 0 || src.length <= fromIndex)
             {
                 throw new IllegalArgumentException(
-                    "OUT OF RANGE off(" + off + "): 0 <= off < src.length("
-                    + src.length + ") - size(" + size + ")");
+                    "OUT OF RANGE fromIndex(" + fromIndex + "): 0 <= fromIndex < src.length(" + src.length + ")");
             }
-            if (src.length < off + size)
+            if (toIndex < fromIndex || src.length < toIndex)
             {
                 throw new IllegalArgumentException(
-                    "FEW SRC LENGTH (" + src.length + "): off(" + off + ") + size("
-                    + size + ") <= src.length");
+                    "OUT OF RANGE toIndex(" + toIndex + "): fromIndex(" + fromIndex + ") <= toIndex <= src.length(" + src.length + ")");
             }
-            int[] mask = new int[size];
-            int[] indexes = new int[size];
-            for (int i = 0; i < size; i++)
-            {
-                mask[i] = rand.nextInt(0x100);
-                indexes[i] = i;
+            int len = toIndex - fromIndex;
+            if (len == 0 || len % blockSize != 0) {
+                throw new IllegalArgumentException(
+                    "OUT OF RANGE toIndex(" + toIndex + "): fromIndex(" + fromIndex + ") <= toIndex <= src.length(" + src.length + ")");
             }
-            for (int i = 0; i < indexes.length; i++)
-            {
-                int j = rand.nextInt(indexes.length - i) + i;
-                int tmp = indexes[i];
-                indexes[i] = indexes[j];
-                indexes[j] = tmp;
-            }
-            byte[] data = new byte[size];
-            for (int i = 0; i < indexes.length; i++)
-            {
-                int j = indexes[i];
-                int b = 0xFF & src[i];
-                b ^= 0xFF & mask[j];
-                data[j] = (byte)b;
-            }
-            DataInputStream dis = new DataInputStream(new ByteArrayInputStream(data));
-            int datasize = size - META_SIZE;
-            dis.skip(datasize);
-            int count = dis.readInt();
-            long code = dis.readLong();
-            dis.close();
-            if (count <= 0 || datasize < count)
-            {
-                // Invalid
-                throw new CryptoException(CryptoException.TYPE_INVALID_COUNT);
-            }
-            checksum.reset();
-            for (int i = 0; i < datasize; i++)
-            {
-                if (i < count)
+            int blockCount = len / blockSize;
+            int dataSize = blockSize - META_SIZE;
+            ByteArrayOutputStream ret = new ByteArrayOutputStream(blockCount * dataSize);
+            int[] mask = new int[blockSize];
+            int[] indexes = new int[blockSize];
+            byte[] data = new byte[blockSize];
+            while (blockCount-- > 0) {
+                for (int i = 0; i < blockSize; i++)
                 {
-                    checksum.update(0xFF & (int)data[i]);
+                    mask[i] = rand.nextInt(BYTE);
+                    indexes[i] = i;
                 }
-                else if (data[i] != 0)
+                for (int i = 0; i < blockSize; i++)
                 {
-                    throw new CryptoException(CryptoException.TYPE_INVALID_DATA);
+                    int j = rand.nextInt(blockSize - i) + i;
+                    int tmp = indexes[i];
+                    indexes[i] = indexes[j];
+                    indexes[j] = tmp;
+                }
+                for (int i = 0; i < blockSize; i++)
+                {
+                    int j = indexes[i];
+                    int b = MASK & src[fromIndex];
+                    b ^= MASK & mask[j];
+                    data[j] = (byte)b;
+                    fromIndex++;
+                }
+                DataInputStream dis = new DataInputStream(new ByteArrayInputStream(data));
+                dis.skip(dataSize);
+                int count = dis.readInt();
+                long code = dis.readLong();
+                dis.close();
+                if (count <= 0 || dataSize < count)
+                {
+                    // Invalid
+                    throw new CryptoException(CryptoException.TYPE_INVALID_COUNT);
+                }
+                checksum.reset();
+                for (int i = 0; i < dataSize; i++)
+                {
+                    if (i < count)
+                    {
+                        ret.write(data[i]);
+                        checksum.update(MASK & (int)data[i]);
+                    }
+                    else if (data[i] != 0)
+                    {
+                        throw new CryptoException(CryptoException.TYPE_INVALID_DATA);
+                    }
+                }
+                if (code != checksum.getValue())
+                {
+                    throw new CryptoException(CryptoException.TYPE_INVALID_CHECKSUM);
                 }
             }
-            if (code != checksum.getValue())
-            {
-                throw new CryptoException(CryptoException.TYPE_INVALID_CHECKSUM);
-            }
-            byte[] ret = new byte[count];
-            System.arraycopy(data, 0, ret, 0, ret.length);
-            return ret;
+            return ret.toByteArray();
         }
         catch (IOException ex)
         {
@@ -142,62 +150,73 @@ public final class Crypto
     }
 
 
-    private static byte[] doEncrypt(int size, Checksum checksum, Random rand, byte[] src, int off, int len)
+    private static byte[] doEncrypt(int blockSize, Checksum checksum, Random rand, byte[] src, int fromIndex, int toIndex)
     {
         try
         {
-            if (off < 0 || src.length <= off)
+            if (fromIndex < 0 || src.length <= fromIndex)
             {
                 throw new IllegalArgumentException(
-                    "OUT OF RANGE off(" + off + "): 0 <= off < src.length("
-                        + src.length + ") - len");
+                    "OUT OF RANGE fromIndex(" + fromIndex + "): 0 <= fromIndex < src.length(" + src.length + ")");
             }
-            if (len <= 0 || src.length < off + len || size - META_SIZE < len)
+            if (toIndex < fromIndex || src.length < toIndex)
             {
                 throw new IllegalArgumentException(
-                    "OUT OF RANGE len(" + len + "): 0 < len <= min(src.length("
-                        + src.length + ") - off(" + off + "), size("
-                        + size + ") - " + META_SIZE + ")");
+                    "OUT OF RANGE toIndex(" + toIndex + "): fromIndex(" + fromIndex + ") <= toIndex <= src.length(" + src.length + ")");
             }
-            int datasize = size - META_SIZE;
-            ByteArrayOutputStream baos = new ByteArrayOutputStream(size);
+            int dataSize = blockSize - META_SIZE;
+            int len = toIndex - fromIndex;
+            int blockCount = (int)(((long)len + (long)dataSize - 1L) / (long)dataSize);
+            long totalSize = (long)blockCount * (long)blockSize;
+            if (totalSize > (long)Integer.MAX_VALUE) {
+                throw new CryptoException(CryptoException.TYPE_INVALID_DATASIZE);
+            }
+            byte[] ret = new byte[(int)totalSize];
+            int pos = 0;
+            ByteArrayOutputStream baos = new ByteArrayOutputStream(blockSize);
             DataOutputStream dos = new DataOutputStream(baos);
-            checksum.reset();
-            int count = 0;
-            for (; count < datasize; count++)
-            {
-                if (count >= len)
+            do {
+                baos.reset();
+                checksum.reset();
+                int count = 0;
+                for (; count < dataSize; count++)
                 {
-                    break;
+                    if (fromIndex + count >= toIndex)
+                    {
+                        break;
+                    }
+                    int b = MASK & (int)src[fromIndex];
+                    checksum.update(b);
+                    b ^= MASK & rand.nextInt(BYTE);
+                    dos.writeByte(b);
+                    fromIndex++;
                 }
-                int b = 0xFF & (int)src[off + count];
-                checksum.update(b);
-                b ^= 0xFF & rand.nextInt(0x100);
-                dos.writeByte(b);
-            }
-            for (int i = count; i < datasize; i++)
-            {
-                int b = 0xFF & rand.nextInt(0x100);
-                dos.writeByte(b);
-            }
-            dos.writeInt(count);
-            dos.writeLong(checksum.getValue());
-            byte[] memory = baos.toByteArray();
-            dos.close();
-            for (int i = datasize; i < memory.length; i++)
-            {
-                int b = 0xFF & (int)memory[i];
-                b ^= 0xFF & rand.nextInt(0x100);
-                memory[i] = (byte)b;
-            }
-            for (int i = 0; i < memory.length; i++)
-            {
-                int j = rand.nextInt(memory.length - i) + i;
-                byte tmp = memory[i];
-                memory[i] = memory[j];
-                memory[j] = tmp;
-            }
-            return memory;
+                for (int i = count; i < dataSize; i++)
+                {
+                    int b = MASK & rand.nextInt(BYTE);
+                    dos.writeByte(b);
+                }
+                dos.writeInt(count);
+                dos.writeLong(checksum.getValue());
+                byte[] memory = baos.toByteArray();
+                dos.close();
+                for (int i = dataSize; i < memory.length; i++)
+                {
+                    int b = MASK & (int)memory[i];
+                    b ^= MASK & rand.nextInt(BYTE);
+                    memory[i] = (byte)b;
+                }
+                for (int i = 0; i < memory.length; i++)
+                {
+                    int j = rand.nextInt(memory.length - i) + i;
+                    byte tmp = memory[i];
+                    memory[i] = memory[j];
+                    memory[j] = tmp;
+                }
+                System.arraycopy(memory, 0, ret, pos, memory.length);
+                pos += memory.length;
+            } while (fromIndex < toIndex);
+            return ret;
         }
         catch (IOException ex)
         {
