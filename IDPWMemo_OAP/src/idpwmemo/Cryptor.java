6782358b0e171cd78efced7d62d98806bf2c6ca9
Leonardone <neetsdkasu@gmail.com>
2021-02-02 17:12:36 +0900

    暗号化方法の変更の反映
    IDPWMemoで更新したissue #13 の対応策の反映

--- IDPWMemo_OAP/src/idpwmemo/Cryptor.java
+++ IDPWMemo_OAP/src/idpwmemo/Cryptor.java
@@ -12,6 +12,7 @@ import neetsdkasu.crypto.CryptoException;
 
 final class Cryptor
 {
+    static final int VERSION = 2;
     static final int MAX_BLOCKSIZE = Math.min(1024, Crypto.MAX_BLOCKSIZE);
     static final String CHARSET = "UTF-8";
 
@@ -32,7 +33,7 @@ final class Cryptor
 
     Cryptor() {}
 
-    private static long[] genSeed(int size)
+    private static long[] genSeedV1(int size)
     {
         long[] seed = new long[size];
         seed[0] = 0x98765432L;
@@ -45,13 +46,13 @@ final class Cryptor
         return seed;
     }
 
-    private static long[] genSeed(byte[] password)
+    private static long[] genSeedV1(byte[] password)
     {
         if (password == null || password.length == 0)
         {
-            return genSeed(23);
+            return genSeedV1(23);
         }
-        long[] seed = genSeed(password.length + 13);
+        long[] seed = genSeedV1(password.length + 13);
         int p = 0;
         for (int i = 0; i < seed.length; i++)
         {
@@ -90,16 +91,16 @@ final class Cryptor
         return size;
     }
 
-    byte[] decrypt(String password, byte[] src) throws IOException
+    byte[] decryptV1(String password, byte[] src) throws IOException
     {
-        return decrypt(getBytes(password), src);
+        return decryptV1(getBytes(password), src);
     }
 
-    synchronized byte[] decrypt(byte[] password, byte[] src) throws IOException
+    synchronized byte[] decryptV1(byte[] password, byte[] src) throws IOException
     {
         ByteArrayInputStream in = new ByteArrayInputStream(src);
         ByteArrayOutputStream out = new ByteArrayOutputStream();
-        long[] seed = genSeed(password);
+        long[] seed = genSeedV1(password);
         for (int size = Math.min(src.length, MAX_BLOCKSIZE); size >= Crypto.MIN_BLOCKSIZE; size--)
         {
             if (src.length % size != 0)
@@ -129,17 +130,17 @@ final class Cryptor
         return null;
     }
 
-    byte[] encrypt(String password, byte[] src) throws IOException
+    byte[] encryptV1(String password, byte[] src) throws IOException
     {
-        return encrypt(getBytes(password), src);
+        return encryptV1(getBytes(password), src);
     }
 
-    synchronized byte[] encrypt(byte[] password, byte[] src) throws IOException
+    synchronized byte[] encryptV1(byte[] password, byte[] src) throws IOException
     {
         int blockSize = encryptBlockSize(src.length);
         ByteArrayInputStream in = new ByteArrayInputStream(src);
         ByteArrayOutputStream out = new ByteArrayOutputStream();
-        rand.setSeed(genSeed(password));
+        rand.setSeed(genSeedV1(password));
         Crypto.encrypt(blockSize, cs, rand, in, out);
         byte[] ret = out.toByteArray();
         in.close();
@@ -147,16 +148,16 @@ final class Cryptor
         return ret;
     }
 
-    byte[] decryptRepeat(int times, String password, byte[] src) throws IOException
+    byte[] decryptRepeatV1(int times, String password, byte[] src) throws IOException
     {
-        return decryptRepeat(times, getBytes(password), src);
+        return decryptRepeatV1(times, getBytes(password), src);
     }
 
-    byte[] decryptRepeat(int times, byte[] password, byte[] src) throws IOException
+    byte[] decryptRepeatV1(int times, byte[] password, byte[] src) throws IOException
     {
         for (int i = 0; i < times; i++)
         {
-            src = decrypt(password, src);
+            src = decryptV1(password, src);
             if (src == null)
             {
                 return null;
@@ -165,17 +166,206 @@ final class Cryptor
         return src;
     }
 
-    byte[] encryptRepeat(int times, String password, byte[] src) throws IOException
+    byte[] encryptRepeatV1(int times, String password, byte[] src) throws IOException
     {
-        return encryptRepeat(times, getBytes(password), src);
+        return encryptRepeatV1(times, getBytes(password), src);
     }
 
-    byte[] encryptRepeat(int times, byte[] password, byte[] src) throws IOException
+    byte[] encryptRepeatV1(int times, byte[] password, byte[] src) throws IOException
     {
         for (int i = 0; i < times; i++)
         {
-            src = encrypt(password, src);
+            src = encryptV1(password, src);
         }
         return src;
+    }
+
+    private static long[] genSeedV2(int size)
+    {
+        long[] seed = new long[size];
+        seed[0] = 0x98765432L;
+        seed[1] = 0xF1E2D3C4L;
+        for (int i = 2; i < seed.length; i++)
+        {
+            seed[i] = seed[i - 2] ^ (seed[i - 1] >>> ((i - 1) & 0xF)) ^ (seed[i - 1] << ((i + i + 1) & 0xF));
+            seed[i] &= 0xFFFFFFFFL;
+        }
+        return seed;
+    }
+
+    private static long[] genSeedV2(byte[] password)
+    {
+        if (password == null || password.length == 0)
+        {
+            return genSeedV2(37);
+        }
+        long[] seed = genSeedV2(password.length + 29);
+        int p = 0;
+        for (int i = 0; i < seed.length; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                seed[i] ^= (0xFF & (int)password[p]) << (((j + i * i) & 3) << 3);
+                p++;
+                if (p >= password.length)
+                {
+                    p = 0;
+                }
+            }
+        }
+        return seed;
+    }
+
+    static int checkSrcType(byte[] src)
+    {
+        int srcType = 0;
+        int version = 0;
+        for (int i = 0; i < 8; i++)
+        {
+            version ^= (int)src[i] & 0xFF;
+        }
+        if (version == VERSION)
+        {
+            int dataLen = src.length-1;
+            for (int size = Math.min(dataLen, MAX_BLOCKSIZE); size >= Crypto.MIN_BLOCKSIZE; size--)
+            {
+                if (dataLen % size == 0)
+                {
+                    srcType = 2;
+                    break;
+                }
+            }
+        }
+        for (int size = Math.min(src.length, MAX_BLOCKSIZE); size >= Crypto.MIN_BLOCKSIZE; size--)
+        {
+            if (src.length % size == 0)
+            {
+                return srcType|1;
+            }
+        }
+        return srcType;
+    }
+
+    byte[] decryptV2(String password, byte[] src) throws IOException
+    {
+        return decryptV2(getBytes(password), src);
+    }
+
+    synchronized byte[] decryptV2(byte[] password, byte[] src) throws IOException
+    {
+        if (src.length < Crypto.MIN_BLOCKSIZE + 1)
+        {
+            return null;
+        }
+        int version = 0;
+        for (int i = 0; i < 8; i++)
+        {
+            version ^= (int)src[i] & 0xFF;
+        }
+        if (version != VERSION)
+        {
+            return null;
+        }
+        ByteArrayInputStream in1 = new ByteArrayInputStream(src);
+        ByteArrayOutputStream out1 = new ByteArrayOutputStream(src.length);
+        ByteArrayOutputStream out2 = new ByteArrayOutputStream(src.length);
+        long[] seed = genSeedV2(password);
+        int dataLen = src.length-1;
+        for (int size1 = Math.min(dataLen, MAX_BLOCKSIZE); size1 >= Crypto.MIN_BLOCKSIZE; size1--)
+        {
+            if (dataLen % size1 != 0)
+            {
+                continue;
+            }
+            in1.reset();
+            in1.skip(1);
+            out1.reset();
+            rand.setSeed(seed);
+            byte[] tmp = null;
+            try
+            {
+                Crypto.decrypt(size1, cs, rand, in1, out1);
+                tmp = out1.toByteArray();
+            }
+            catch (CryptoException ex)
+            {
+                continue;
+            }
+            ByteArrayInputStream in2 = new ByteArrayInputStream(tmp);
+            for (int size2 = Math.min(tmp.length, MAX_BLOCKSIZE); size2 >= Crypto.MIN_BLOCKSIZE; size2--)
+            {
+                if (tmp.length % size2 != 0)
+                {
+                    continue;
+                }
+                in2.reset();
+                out2.reset();
+                rand.setSeed(seed);
+                try
+                {
+                    Crypto.decrypt(size2, cs, rand, in2, out2);
+                    byte[] ret = out2.toByteArray();
+                    seed = null;
+                    tmp = null;
+                    in1.close(); in1 = null;
+                    in2.close(); in2 = null;
+                    out1.close(); out1 = null;
+                    out2.close(); out2 = null;
+                    return ret;
+                }
+                catch (CryptoException ex)
+                {
+                    // continue;
+                }
+            }
+            tmp = null;
+            in2.close(); in2 = null;
+        }
+        seed = null;
+        in1.close(); in1 = null;
+        out1.close(); out1 = null;
+        out2.close(); out2 = null;
+        return null;
+    }
+
+    int calcSize(int srclen, int blockSize)
+    {
+        int dataSize = blockSize - Crypto.META_SIZE;
+        int blockCount = (srclen + dataSize - 1) / dataSize;
+        return blockSize * blockCount;
+    }
+
+    byte[] encryptV2(String password, byte[] src) throws IOException
+    {
+        return encryptV2(getBytes(password), src);
+    }
+
+    synchronized byte[] encryptV2(byte[] password, byte[] src) throws IOException
+    {
+        int blockSize = encryptBlockSize(src.length);
+        int size = calcSize(src.length, blockSize);
+        long[] seed = genSeedV2(password);
+        ByteArrayInputStream in = new ByteArrayInputStream(src);
+        ByteArrayOutputStream out = new ByteArrayOutputStream(size);
+        rand.setSeed(seed);
+        Crypto.encrypt(blockSize, cs, rand, in, out);
+        src = out.toByteArray();
+        in.close();
+        out.close();
+        blockSize = encryptBlockSize(src.length);
+        size = calcSize(src.length, blockSize);
+        in = new ByteArrayInputStream(src);
+        out = new ByteArrayOutputStream(size+1);
+        out.write(VERSION);
+        rand.setSeed(seed);
+        Crypto.encrypt(blockSize, cs, rand, in, out);
+        byte[] ret = out.toByteArray();
+        in.close();
+        out.close();
+        for (int i = 1; i < 8; i++)
+        {
+            ret[0] = (byte)((int)ret[0] ^ (int)ret[i]);
+        }
+        return ret;
     }
 }
--- IDPWMemo_OAP/src/idpwmemo/IDPWMemo.java
+++ IDPWMemo_OAP/src/idpwmemo/IDPWMemo.java
@@ -14,7 +14,9 @@ public final class IDPWMemo
     }
 
     Cryptor cryptor = Cryptor.instance;
-    byte[] encodedPassword = null;
+    byte[] encodedPasswordV1 = null;
+    byte[] encodedPasswordV2 = null;
+    int version = 0;
     Memo memo = null;
     int serviceIndex = -1;
     Service service = null;
@@ -24,7 +26,9 @@ public final class IDPWMemo
 
     public void clear()
     {
-        encodedPassword = null;
+        encodedPasswordV1 = null;
+        encodedPasswordV2 = null;
+        version = 0;
         memo = null;
         serviceIndex = -1;
         service = null;
@@ -38,29 +42,42 @@ public final class IDPWMemo
 
     public void setPassword(byte[] password) throws IOException
     {
-        byte[] tmp = cryptor.encrypt(Service.EMPTY_BYTES, password);
-        encodedPassword = tmp;
+        byte[] tmp1 = cryptor.encryptV1(Service.EMPTY_BYTES, password);
+        byte[] tmp2 = cryptor.encryptV2(password, password);
+        encodedPasswordV1 = tmp1;
+        encodedPasswordV2 = tmp2;
+        version = 0;
         memo = null;
         serviceIndex = -1;
         service = null;
         secrets = null;
     }
 
-    byte[] getPassword() throws IOException
+    byte[] getPasswordV1() throws IOException
     {
-        if (encodedPassword == null)
+        if (encodedPasswordV2 == null)
         {
             throw new RuntimeException("no password");
         }
-        return cryptor.decrypt(Service.EMPTY_BYTES, encodedPassword);
+        return cryptor.decryptV1(Service.EMPTY_BYTES, encodedPasswordV1);
     }
 
     public void newMemo()
     {
-        if (encodedPassword == null)
+        newMemo(2);
+    }
+
+    public void newMemo(int version)
+    {
+        if (version < 1 && version > 2)
+        {
+            throw new RuntimeException("wrong version");
+        }
+        if (encodedPasswordV2 == null)
         {
             throw new RuntimeException("no password");
         }
+        this.version = version;
         memo = new Memo();
         serviceIndex = -1;
         service = null;
@@ -69,10 +86,28 @@ public final class IDPWMemo
 
     public boolean loadMemo(byte[] src) throws IOException
     {
-        byte[] password = getPassword();
-        byte[] buf = cryptor.decryptRepeat(2, password, src);
-        password = null;
-        if (buf == null)
+        byte[] buf = null;
+        int st = Cryptor.checkSrcType(src);
+        if ((st&2) != 0)
+        {
+            buf = cryptor.decryptV2(encodedPasswordV2, src);
+        }
+        if (buf != null)
+        {
+            version = 2;
+        }
+        else if ((st&1) != 0)
+        {
+            byte[] password = getPasswordV1();
+            buf = cryptor.decryptRepeatV1(2, password, src);
+            password = null;
+            if (buf == null)
+            {
+                return false;
+            }
+            version = 1;
+        }
+        else
         {
             return false;
         }
@@ -278,9 +313,17 @@ public final class IDPWMemo
             secrets = Service.EMPTY_VALUES;
             return secrets;
         }
-        byte[] password = getPassword();
-        byte[] buf = cryptor.decryptRepeat(2, password, src);
-        password = null;
+        byte[] buf = null;
+        if (version == 1)
+        {
+            byte[] password = getPasswordV1();
+            buf = cryptor.decryptRepeatV1(2, password, src);
+            password = null;
+        }
+        else if (version == 2)
+        {
+            buf = cryptor.decryptV2(encodedPasswordV2, src);
+        }
         src = null;
         if (buf == null)
         {
@@ -359,9 +402,16 @@ public final class IDPWMemo
         dos.flush();
         byte[] buf = baos.toByteArray();
         dos.close();
-        byte[] password = getPassword();
-        service.setSecrets(cryptor.encryptRepeat(2, password, buf));
-        password = null;
+        if (version == 1)
+        {
+            byte[] password = getPasswordV1();
+            service.setSecrets(cryptor.encryptRepeatV1(2, password, buf));
+            password = null;
+        }
+        else if (version == 2)
+        {
+            service.setSecrets(cryptor.encryptV2(encodedPasswordV2, buf));
+        }
         buf = null;
     }
 
@@ -377,7 +427,18 @@ public final class IDPWMemo
         dos.flush();
         byte[] buf = baos.toByteArray();
         dos.close();
-        return cryptor.encryptRepeat(2, getPassword(), buf);
+        if (version == 1)
+        {
+            return cryptor.encryptRepeatV1(2, getPasswordV1(), buf);
+        }
+        else if (version == 2)
+        {
+            return cryptor.encryptV2(encodedPasswordV2, buf);
+        }
+        else
+        {
+            throw new RuntimeException("BUG");
+        }
     }
 
     public void changePassword(String newPassword) throws IOException
@@ -391,7 +452,23 @@ public final class IDPWMemo
         {
             throw new RuntimeException("no memo");
         }
-        byte[] oldPassword = getPassword();
+        if (version == 1)
+        {
+            changePasswordV1(newPassword);
+        }
+        else if (version == 2)
+        {
+            changePasswordV2(newPassword);
+        }
+        else
+        {
+            throw new RuntimeException("BUG");
+        }
+    }
+
+    private void changePasswordV1(byte[] newPassword) throws IOException
+    {
+        byte[] oldPassword = getPasswordV1();
         byte[][] encs = new byte[memo.getServiceCount()][];
         for (int i = 0; i < memo.getServiceCount(); i++)
         {
@@ -402,26 +479,113 @@ public final class IDPWMemo
                 continue;
             }
             byte[] sec = sv.getSecrets();
-            byte[] dec = cryptor.decryptRepeat(2, oldPassword, sec);
+            byte[] dec = cryptor.decryptRepeatV1(2, oldPassword, sec);
             sec = null;
-            encs[i] = cryptor.encryptRepeat(2, newPassword, dec);
+            encs[i] = cryptor.encryptRepeatV1(2, newPassword, dec);
             dec = null;
         }
-        byte[] tmp = cryptor.encrypt(Service.EMPTY_BYTES, newPassword);
+        byte[] tmp1 = cryptor.encryptV1(Service.EMPTY_BYTES, newPassword);
+        byte[] tmp2 = cryptor.encryptV2(newPassword, newPassword);
         if (service != null && service.hasSecrets())
         {
             byte[] sec = service.getSecrets();
-            byte[] dec = cryptor.decryptRepeat(2, oldPassword, sec);
+            byte[] dec = cryptor.decryptRepeatV1(2, oldPassword, sec);
             sec = null;
-            byte[] enc = cryptor.encryptRepeat(2, newPassword, dec);
+            byte[] enc = cryptor.encryptRepeatV1(2, newPassword, dec);
             dec = null;
             service.setSecrets(enc);
             enc = null;
         }
-        encodedPassword = tmp;
+        encodedPasswordV1 = tmp1;
+        encodedPasswordV2 = tmp2;
         for (int i = 0; i < memo.getServiceCount(); i++)
         {
             memo.getService(i).setSecrets(encs[i]);
         }
+        oldPassword = null;
+        tmp1 = null;
+        tmp2 = null;
+        encs = null;
+    }
+
+    private void changePasswordV2(byte[] newPassword) throws IOException
+    {
+        byte[] tmpPasswordV2 = cryptor.encryptV2(newPassword, newPassword);
+        byte[][] encs = new byte[memo.getServiceCount()][];
+        for (int i = 0; i < memo.getServiceCount(); i++)
+        {
+            Service sv = memo.getService(i);
+            if (!sv.hasSecrets())
+            {
+                encs[i] = Service.EMPTY_BYTES;
+                continue;
+            }
+            byte[] sec = sv.getSecrets();
+            byte[] dec = cryptor.decryptV2(encodedPasswordV2, sec);
+            sec = null;
+            encs[i] = cryptor.encryptV2(tmpPasswordV2, dec);
+            dec = null;
+        }
+        byte[] tmpPasswordV1 = cryptor.encryptV1(Service.EMPTY_BYTES, newPassword);
+        if (service != null && service.hasSecrets())
+        {
+            byte[] sec = service.getSecrets();
+            byte[] dec = cryptor.decryptV2(encodedPasswordV2, sec);
+            sec = null;
+            byte[] enc = cryptor.encryptV2(tmpPasswordV2, dec);
+            dec = null;
+            service.setSecrets(enc);
+            enc = null;
+        }
+        encodedPasswordV1 = tmpPasswordV1;
+        encodedPasswordV2 = tmpPasswordV2;
+        for (int i = 0; i < memo.getServiceCount(); i++)
+        {
+            memo.getService(i).setSecrets(encs[i]);
+        }
+        tmpPasswordV1 = null;
+        tmpPasswordV2 = null;
+        encs = null;
+    }
+
+    public void convertV1ToV2() throws IOException
+    {
+        if (version != 1)
+        {
+            return;
+        }
+        byte[] oldPassword = getPasswordV1();
+        byte[][] encs = new byte[memo.getServiceCount()][];
+        for (int i = 0; i < memo.getServiceCount(); i++)
+        {
+            Service sv = memo.getService(i);
+            if (!sv.hasSecrets())
+            {
+                encs[i] = Service.EMPTY_BYTES;
+                continue;
+            }
+            byte[] sec = sv.getSecrets();
+            byte[] dec = cryptor.decryptRepeatV1(2, oldPassword, sec);
+            sec = null;
+            encs[i] = cryptor.encryptV2(encodedPasswordV2, dec);
+            dec = null;
+        }
+        if (service != null && service.hasSecrets())
+        {
+            byte[] sec = service.getSecrets();
+            byte[] dec = cryptor.decryptRepeatV1(2, oldPassword, sec);
+            sec = null;
+            byte[] enc = cryptor.encryptV2(encodedPasswordV2, dec);
+            dec = null;
+            service.setSecrets(enc);
+            enc = null;
+        }
+        for (int i = 0; i < memo.getServiceCount(); i++)
+        {
+            memo.getService(i).setSecrets(encs[i]);
+        }
+        version = 2;
+        oldPassword = null;
+        encs = null;
     }
 }
