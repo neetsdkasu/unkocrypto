2451ab93d5ab27c99c24c63acd37e09eccc021e5
Leonardone <neetsdkasu@gmail.com>
2023-06-12 02:13:35 +0900

    ひとまずビルドはできる空のプロジェクトの状態・・・
    gradlew buildでビルドできる、はず

---
+++ IDPWMemo10/README.md
@@ -0,0 +1,9 @@
+# IDPWMemo10 (android 10)
+
+
+    id: "android-29"
+    Name: Android 10
+    Type: Platform
+    API level: 29
+    Revision: 4
+
---
+++ IDPWMemo10/src/main/java/neetsdkasu/idpwmemo10/MainActivity.java
@@ -0,0 +1,18 @@
+package neetsdkasu.idpwmemo10;
+
+import android.app.Activity;
+import android.os.Bundle;
+import androidx.fragment.app.DialogFragment;
+
+public class MainActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        DialogFragment f = null;
+    }
+}
