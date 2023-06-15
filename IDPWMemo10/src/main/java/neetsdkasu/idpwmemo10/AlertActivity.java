8e2f6d36b65336260971c8063394fcfc3e0a81eb
Leonardone <neetsdkasu@gmail.com>
2023-06-16 04:17:42 +0900

    Alertぽくないが、Activityで代用･･･
    DialogFragmentを使えないこの開発環境が悪いのだ･･･
    最新の開発環境さえ整えられれば･･･

---
+++ IDPWMemo10/src/main/java/neetsdkasu/idpwmemo10/AlertActivity.java
@@ -0,0 +1,14 @@
+package neetsdkasu.idpwmemo10;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AlertActivity extends Activity
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.alert);
+    }
+}
--- IDPWMemo10/src/main/java/neetsdkasu/idpwmemo10/MainActivity.java
+++ IDPWMemo10/src/main/java/neetsdkasu/idpwmemo10/MainActivity.java
@@ -1,15 +1,25 @@
 package neetsdkasu.idpwmemo10;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity
 {
-    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        Button button = findViewById(R.id.alert_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent alert = new Intent(MainActivity.this, AlertActivity.class);
+                MainActivity.this.startActivity(alert);
+            }
+        });
     }
 }
