507e8768933099b0b986fde7f9191050cdf91b10
Leonardone <neetsdkasu@gmail.com>
2023-06-20 05:25:46 +0900

    ListViewというのとコンテキストメニューというのを使ってみた

--- IDPWMemo10/src/main/java/neetsdkasu/idpwmemo10/MainActivity.java
+++ IDPWMemo10/src/main/java/neetsdkasu/idpwmemo10/MainActivity.java
@@ -7,25 +7,41 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.ParcelFileDescriptor;
 import android.provider.OpenableColumns;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-// import android.widget.Toast;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import mt19937ar.MTRandom;
 
 public class MainActivity extends Activity
 {
-    static final int REQ_IMPORT = 10;
-    static final int REQ_EXPORT = 20;
+    private static final int REQ_IMPORT = 10;
+    private static final int REQ_EXPORT = 20;
 
-    int state = 0;
+    private static final int STATE_IDLE            = 0;
+    private static final int STATE_REQ_IMPORT      = 10;
+    private static final int STATE_SUCCESS_IMPORT  = 11;
+    private static final int STATE_FAILURE_IMPORT  = 12;
+    private static final int STATE_CANCELED_IMPORT = 13;
+    private static final int STATE_REQ_EXPORT      = 20;
+    private static final int STATE_SUCCESS_EXPORT  = 21;
+    private static final int STATE_FAILURE_EXPORT  = 22;
+    private static final int STATE_CANCELED_EXPORT = 23;
+
+    private int state = MainActivity.STATE_IDLE;
+
+    private ArrayAdapter<String> listAdapter = null;
 
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -33,29 +49,46 @@ public class MainActivity extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        Button alertButton = findViewById(R.id.alert_button);
-        alertButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent alert = new Intent(MainActivity.this, AlertActivity.class);
-                MainActivity.this.startActivity(alert);
-            }
-        });
+        List<String> memoList = new ArrayList<>();
 
-        Button importButton = findViewById(R.id.import_button);
-        importButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                MainActivity.this.openImportMemoFile();
-            }
-        });
+        listAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, memoList);
 
-        Button exportButton = findViewById(R.id.export_button);
-        exportButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                MainActivity.this.createExportFile();
+        ListView listView = findViewById(R.id.memo_list_view);
+        listView.setAdapter(listAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String fileName = MainActivity.this.listAdapter.getItem(position);
+                Toast.makeText(MainActivity.this, fileName, Toast.LENGTH_SHORT).show();
             }
         });
+        registerForContextMenu(listView);
     }
 
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.memo_list_context_menu, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        int id = item.getItemId();
+        if (id == R.id.change_password_menu_item) {
+            // TODO
+            return true;
+        } else if (id == R.id.export_memo_menu_item) {
+            String fileName = this.listAdapter.getItem(info.position);
+            this.createExportFile(fileName);
+            return true;
+        } else if (id == R.id.delete_memo_menu_item) {
+            // TODO
+            return true;
+        } else {
+            return super.onContextItemSelected(item);
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -82,37 +115,37 @@ public class MainActivity extends Activity
     protected void onResume() {
         super.onResume();
 
-        final TextView helloTextView = findViewById(R.id.hello_text_view);
+        this.showStateMessage();
+    }
+
+    private void showStateMessage() {
         switch (this.state) {
-            case 10:
-                helloTextView.setText(R.string.import_button_text);
+            case MainActivity.STATE_REQ_IMPORT:
+                return;
+            case MainActivity.STATE_SUCCESS_IMPORT:
+                Toast.makeText(this, R.string.imported, Toast.LENGTH_SHORT).show();
                 break;
-            case 11:
-                helloTextView.setText(R.string.imported);
+            case MainActivity.STATE_FAILURE_IMPORT:
+                Toast.makeText(this, R.string.import_error, Toast.LENGTH_SHORT).show();
                 break;
-            case 12:
-                helloTextView.setText(R.string.import_error);
+            case MainActivity.STATE_CANCELED_IMPORT:
+                Toast.makeText(this, R.string.import_canceled, Toast.LENGTH_SHORT).show();
                 break;
-            case 13:
-                helloTextView.setText(R.string.import_canceled);
+            case MainActivity.STATE_REQ_EXPORT:
+                return;
+            case MainActivity.STATE_SUCCESS_EXPORT:
+                Toast.makeText(this, R.string.exported, Toast.LENGTH_SHORT).show();
                 break;
-            case 20:
-                helloTextView.setText(R.string.export_button_text);
+            case MainActivity.STATE_FAILURE_EXPORT:
+                Toast.makeText(this, R.string.export_error, Toast.LENGTH_SHORT).show();
                 break;
-            case 21:
-                helloTextView.setText(R.string.exported);
-                break;
-            case 22:
-                helloTextView.setText(R.string.export_error);
-                break;
-            case 23:
-                helloTextView.setText(R.string.export_canceled);
+            case MainActivity.STATE_CANCELED_EXPORT:
+                Toast.makeText(this, R.string.export_canceled, Toast.LENGTH_SHORT).show();
                 break;
             default:
-                helloTextView.setText(R.string.hello);
                 break;
         }
-        this.state = 0;
+        this.state = MainActivity.STATE_IDLE;
     }
 
     @Override
@@ -122,67 +155,64 @@ public class MainActivity extends Activity
                 Uri uri = data.getData();
                 this.importMemoFile(uri);
             } else {
-                this.state = 13;
+                this.state = MainActivity.STATE_CANCELED_IMPORT;
             }
         } else if (requestCode == MainActivity.REQ_EXPORT) {
             if (resultCode == RESULT_OK) {
                 Uri uri = data.getData();
                 this.exportMemoFile(uri);
             } else {
-                this.state = 23;
+                this.state = MainActivity.STATE_CANCELED_EXPORT;
             }
         }
     }
 
-    int addNewMemoCount = 0;
+    private int addNewMemoCount = 0;
 
-    void addNewMemo() {
-        final TextView importFileTextView = findViewById(R.id.import_file_text_view);
+    private void addNewMemo() {
         this.addNewMemoCount++;
         MTRandom rand = new MTRandom(this.addNewMemoCount);
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < 8; i++) {
-            sb.append('A' + (char)(rand.nextInt() % 5));
+            sb.append((char)((int)'A' + (rand.nextInt() % 20)));
         }
         sb.append(".memo");
-        importFileTextView.setText(sb.toString());
+        this.listAdapter.add(sb.toString());
     }
 
-    void openImportMemoFile() {
+    private void openImportMemoFile() {
         Intent importMemo = new Intent(Intent.ACTION_GET_CONTENT);
         importMemo.setType("*/*");
         importMemo.addCategory(Intent.CATEGORY_OPENABLE);
         importMemo.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
         startActivityForResult(importMemo, MainActivity.REQ_IMPORT);
-        this.state = 10;
+        this.state = MainActivity.STATE_REQ_IMPORT;
     }
 
-    void createExportFile() {
-        final TextView exportFileTextView = findViewById(R.id.export_file_text_view);
+    private void createExportFile(String fileName) {
         Intent exportMemo = new Intent(Intent.ACTION_CREATE_DOCUMENT);
         exportMemo.setType("application/octet-stream");
         exportMemo.addCategory(Intent.CATEGORY_OPENABLE);
-        CharSequence filename = exportFileTextView.getText();
-        exportMemo.putExtra(Intent.EXTRA_TITLE, filename);
+        exportMemo.putExtra(Intent.EXTRA_TITLE, fileName);
         startActivityForResult(exportMemo, MainActivity.REQ_EXPORT);
-        this.state = 20;
+        this.state = MainActivity.STATE_REQ_EXPORT;
     }
 
-    void importMemoFile(Uri uri) {
+    private void importMemoFile(Uri uri) {
         try {
             try (Cursor returnCursor = getContentResolver().query(uri, null, null, null, null)) {
                 int nameIndex = returnCursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
                 returnCursor.moveToFirst();
-                final TextView importFileTextView = findViewById(R.id.import_file_text_view);
-                importFileTextView.setText(returnCursor.getString(nameIndex));
-                this.state = 11;
+                String fileName = returnCursor.getString(nameIndex);
+                this.listAdapter.add(fileName);
+                this.state = MainActivity.STATE_SUCCESS_IMPORT;
             }
         } catch (Exception ex) {
-            this.state = 12;
+            this.state = MainActivity.STATE_FAILURE_IMPORT;
         }
     }
 
-    void exportMemoFile(Uri uri) {
+    private void exportMemoFile(Uri uri) {
         try {
             try (ParcelFileDescriptor pfd = getContentResolver().openFileDescriptor(uri, "w")) {
                 try (FileOutputStream out = new FileOutputStream(pfd.getFileDescriptor())) {
@@ -191,11 +221,11 @@ public class MainActivity extends Activity
                     rand.nextBytes(buf);
                     out.write(buf);
                     out.flush();
-                    this.state = 21;
+                    this.state = MainActivity.STATE_SUCCESS_EXPORT;
                 }
             }
         } catch (Exception ex) {
-            this.state = 22;
+            this.state = MainActivity.STATE_FAILURE_EXPORT;
         }
     }
 }
