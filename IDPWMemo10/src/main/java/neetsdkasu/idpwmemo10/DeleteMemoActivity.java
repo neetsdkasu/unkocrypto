df3acbd4a6bfe839c5af20712f471d0ac98daf1d
Leonardone <neetsdkasu@gmail.com>
2023-08-19 17:19:25 +0900

    udpate DeleteMemoActivity

--- IDPWMemo10/src/main/java/neetsdkasu/idpwmemo10/DeleteMemoActivity.java
+++ IDPWMemo10/src/main/java/neetsdkasu/idpwmemo10/DeleteMemoActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.TextView;
@@ -14,39 +15,54 @@ public class DeleteMemoActivity extends Activity {
 
     static final String INTENT_EXTRA_MEMO_NAME = "neetsdkasu.idpwmemo10.DeleteMemoActivity.INTENT_EXTRA_MEMO_NAME";
 
+    // 想定のIntentを受け取ったときtrueでアプリは正常状態、それ以外falseでアプリは異常状態
+    private boolean statusOk = false;
+
+    private String memoName = "";
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.delete_memo);
 
-        String memoName = "";
-
         Intent intent = getIntent();
-        if (intent != null) {
-            memoName = Utils.ifNullToBlank(intent.getStringExtra(DeleteMemoActivity.INTENT_EXTRA_MEMO_NAME));
+
+        this.statusOk = intent != null
+            && intent.hasExtra(DeleteMemoActivity.INTENT_EXTRA_MEMO_NAME);
+
+        if (this.statusOk) {
+
+            this.memoName = intent.getStringExtra(DeleteMemoActivity.INTENT_EXTRA_MEMO_NAME);
+
+            this.statusOk = !Utils.isNullOrBlank(this.memoName);
         }
 
-        TextView memoNameTextView = findViewById(R.id.delete_memo_memo_name);
-        memoNameTextView.setText(memoName);
+        if (this.statusOk) {
+
+            TextView memoNameTextView = findViewById(R.id.delete_memo_memo_name);
+            memoNameTextView.setText(this.memoName);
+
+        } else {
+
+            setTitle(R.string.common_text_status_error_title);
+            Button executeButton = findViewById(R.id.delete_memo_execute_button);
+            executeButton.setEnabled(false);
+            Switch executeSwitch = findViewById(R.id.delete_memo_execute_switch);
+            executeSwitch.setEnabled(false);
+
+        }
     }
 
     // res/layout/delete_memo.xml Button onClick
     public void onClickOkButton(View v) {
+        if (!this.statusOk) {
+            Utils.alertShort(this, R.string.msg_internal_error);
+            return;
+        }
 
         this.hideInputMethod();
 
-        Intent intent = getIntent();
-        if (intent == null) {
-            Utils.alertShort(this, R.string.msg_internal_error);
-            return;
-        }
-        String memoName = intent.getStringExtra(DeleteMemoActivity.INTENT_EXTRA_MEMO_NAME);
-        if (memoName == null) {
-            Utils.alertShort(this, R.string.msg_internal_error);
-            return;
-        }
-
-        File memoFile = Utils.getMemoFile(this, memoName);
+        File memoFile = Utils.getMemoFile(this, this.memoName);
         if (!memoFile.exists()) {
             Utils.alertShort(this, R.string.msg_internal_error);
             return;
