26bfdc5d2130b4a3b1e4b20969ef02b36c3f4cb3
Leonardone <neetsdkasu@gmail.com>
2023-08-19 17:58:22 +0900

    update DeleteServiceAtivity

--- IDPWMemo10/src/main/java/neetsdkasu/idpwmemo10/DeleteServiceActivity.java
+++ IDPWMemo10/src/main/java/neetsdkasu/idpwmemo10/DeleteServiceActivity.java
@@ -14,6 +14,9 @@ public class DeleteServiceActivity extends Activity {
     static final String INTENT_EXTRA_SERVICE_NAME = "neetsdkasu.idpwmemo10.DeleteServiceActivity.INTENT_EXTRA_SERVICE_NAME";
     static final String INTENT_EXTRA_LASTUPDATE   = "neetsdkasu.idpwmemo10.DeleteServiceActivity.INTENT_EXTRA_LASTUPDATE";
 
+    // 想定のIntentを受け取ったときtrueでアプリは正常状態、それ以外falseでアプリは異常状態
+    private boolean statusOk = false;
+
     private int index = -1;
     private String serviceName = "";
     private long lastupdate = 0L;
@@ -24,17 +27,38 @@ public class DeleteServiceActivity extends Activity {
         setContentView(R.layout.delete_service);
 
         Intent intent = getIntent();
-        if (intent != null) {
+
+        this.statusOk = intent != null
+            && intent.hasExtra(DeleteServiceActivity.INTENT_EXTRA_INDEX)
+            && intent.hasExtra(DeleteServiceActivity.INTENT_EXTRA_SERVICE_NAME)
+            && intent.hasExtra(DeleteServiceActivity.INTENT_EXTRA_LASTUPDATE);
+
+        if (this.statusOk) {
+
             this.index = intent.getIntExtra(DeleteServiceActivity.INTENT_EXTRA_INDEX, -1);
-            this.serviceName = Utils.ifNullToBlank(intent.getStringExtra(DeleteServiceActivity.INTENT_EXTRA_SERVICE_NAME));
-            this.lastupdate = intent.getLongExtra(DeleteServiceActivity.INTENT_EXTRA_LASTUPDATE, 0L);
+            this.serviceName = intent.getStringExtra(DeleteServiceActivity.INTENT_EXTRA_SERVICE_NAME);
+            this.lastupdate = intent.getLongExtra(DeleteServiceActivity.INTENT_EXTRA_LASTUPDATE, -1L);
+
+            this.statusOk = this.index >= 0
+                && Utils.isNotBlank(this.serviceName)
+                && this.lastupdate >= 0L;
         }
 
-        TextView serviceNameTextView = findViewById(R.id.delete_service_name);
-        serviceNameTextView.setText(this.serviceName);
+        if (this.statusOk) {
 
-        TextView lastupdateTextView = findViewById(R.id.delete_service_lastupdate);
-        lastupdateTextView.setText(Utils.formatDate(this.lastupdate));
+            TextView serviceNameTextView = findViewById(R.id.delete_service_name);
+            serviceNameTextView.setText(this.serviceName);
+
+            TextView lastupdateTextView = findViewById(R.id.delete_service_lastupdate);
+            lastupdateTextView.setText(Utils.formatDate(this.lastupdate));
+
+        } else {
+
+            setTitle(R.string.common_text_status_error_title);
+            findViewById(R.id.delete_service_execute_button).setEnabled(false);
+            findViewById(R.id.delete_service_execute_switch).setEnabled(false);
+
+        }
 
         Window window = getWindow();
         if (window != null) {
@@ -44,6 +68,10 @@ public class DeleteServiceActivity extends Activity {
 
     // res/layout/delete_service.xml Button onClick
     public void onClickOkButton(View v) {
+        if (!this.statusOk) {
+            Utils.alertShort(this, R.string.msg_internal_error);
+            return;
+        }
 
         Switch executeSwitch = findViewById(R.id.delete_service_execute_switch);
         if (!executeSwitch.isChecked()) {
