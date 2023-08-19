71fc4a752bc215f0fd8271ccbe616475f83ba69b
Leonardone <neetsdkasu@gmail.com>
2023-08-19 16:33:01 +0900

    update ChangeMemoKeywordActivity

--- IDPWMemo10/src/main/java/neetsdkasu/idpwmemo10/ChangeMemoKeywordActivity.java
+++ IDPWMemo10/src/main/java/neetsdkasu/idpwmemo10/ChangeMemoKeywordActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.TextView;
@@ -16,6 +17,9 @@ public class ChangeMemoKeywordActivity extends Activity {
 
     static final String INTENT_EXTRA_MEMO_NAME = "neetsdkasu.idpwmemo10.ChangeMemoKeywordActivity.INTENT_EXTRA_MEMO_NAME";
 
+    // 想定のIntentを受け取ったときtrueでアプリは正常状態、それ以外falseでアプリは異常状態
+    private boolean statusOk = false;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,16 +28,34 @@ public class ChangeMemoKeywordActivity extends Activity {
         String memoName = "";
 
         Intent intent = getIntent();
-        if (intent != null) {
-            memoName = Utils.ifNullToBlank(intent.getStringExtra(ChangeMemoKeywordActivity.INTENT_EXTRA_MEMO_NAME));
-        }
 
-        TextView memoNameTextView = findViewById(R.id.change_memo_keyword_memo_name);
-        memoNameTextView.setText(memoName);
+        this.statusOk = intent != null
+            && intent.hasExtra(ChangeMemoKeywordActivity.INTENT_EXTRA_MEMO_NAME);
+
+        if (this.statusOk) {
+
+            memoName = Utils.ifNullToBlank(intent.getStringExtra(ChangeMemoKeywordActivity.INTENT_EXTRA_MEMO_NAME));
+
+            TextView memoNameTextView = findViewById(R.id.change_memo_keyword_memo_name);
+            memoNameTextView.setText(memoName);
+
+        } else {
+
+            setTitle(R.string.common_text_status_error_title);
+            Button excuteButton = findViewById(R.id.change_memo_keyword_execute_button);
+            excuteButton.setEnabled(false);
+            Switch executeSwitch = findViewById(R.id.change_memo_keyword_execute_switch);
+            executeSwitch.setEnabled(false);
+
+        }
     }
 
     // res/layout/change_memo_keyword.xml Button onClick
     public void onClickOkButton(View v) {
+        if (!this.statusOk) {
+            Utils.alertShort(this, R.string.msg_internal_error);
+            return;
+        }
 
         this.hideInputMethod();
 
