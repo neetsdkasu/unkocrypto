6cd368e3799b8acaa2eedfc33f8cf4712ddf136b
Leonardone <neetsdkasu@gmail.com>
2023-08-20 03:20:47 +0900

    update NewServiceActivity. 不要なimport文を除去した

--- IDPWMemo10/src/main/java/neetsdkasu/idpwmemo10/NewServiceActivity.java
+++ IDPWMemo10/src/main/java/neetsdkasu/idpwmemo10/NewServiceActivity.java
@@ -5,8 +5,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-import java.io.File;
-import java.nio.file.Files;
 
 public class NewServiceActivity extends Activity {
 
