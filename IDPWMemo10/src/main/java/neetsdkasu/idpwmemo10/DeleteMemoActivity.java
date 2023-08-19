1572edc980c2a4e02c916afb3ee00bbfe10c9e4d
Leonardone <neetsdkasu@gmail.com>
2023-08-19 17:35:57 +0900

    fix bug? DeleteMemoActivity

--- IDPWMemo10/src/main/java/neetsdkasu/idpwmemo10/DeleteMemoActivity.java
+++ IDPWMemo10/src/main/java/neetsdkasu/idpwmemo10/DeleteMemoActivity.java
@@ -34,7 +34,8 @@ public class DeleteMemoActivity extends Activity {
 
             this.memoName = intent.getStringExtra(DeleteMemoActivity.INTENT_EXTRA_MEMO_NAME);
 
-            this.statusOk = !Utils.isNullOrBlank(this.memoName);
+            this.statusOk = !Utils.isNullOrBlank(this.memoName)
+                && Utils.getMemoFile(this, this.memoName).exists();
         }
 
         if (this.statusOk) {
