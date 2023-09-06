5442e0b1d4e88e38362eaf0d1d5c7fce80fe7564
Leonardone <neetsdkasu@gmail.com>
2023-09-07 03:16:01 +0900

    タイムリミットを１分から３０分に変更
    １分はテスト用の長さ（１分でも長いが・・・
    
    ３０分は・・・適切かどうか微妙

--- IDPWMemo10/src/main/java/neetsdkasu/idpwmemo10/TimeLimitChecker.java
+++ IDPWMemo10/src/main/java/neetsdkasu/idpwmemo10/TimeLimitChecker.java
@@ -2,10 +2,10 @@ package neetsdkasu.idpwmemo10;
 
 class TimeLimitChecker {
     // 30 minute. (== 1800 seconds == 1800000 milli-seconds)
-    // static final long TIME_LIMIT = 30L * 60L * 1000L;
+    static final long TIME_LIMIT = 30L * 60L * 1000L;
 
     // FOR TEST a minute. (== 60 seconds == 60000 milli-seconds)
-    static final long TIME_LIMIT = 1L * 60L * 1000L;
+    // static final long TIME_LIMIT = 1L * 60L * 1000L;
 
     long timeLimit = 0L;
 
